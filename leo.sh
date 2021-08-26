24dc1bb856446709268670fbb959b227cf56c0b1
orzwalker <52889249+orzwalker@users.noreply.github.com>
2021-08-26 09:15:14 +0800

    redis auto commit

--- leo.sh
+++ leo.sh
@@ -3,6 +3,6 @@ current_path=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 cd current_path
 git add .
 git commit -m "redis auto commit"
-git push origin master 
+git push origin 6.2 
 echo "push successful"
 
